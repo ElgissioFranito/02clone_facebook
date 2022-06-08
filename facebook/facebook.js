d0c8f58ebbc7907389f541da1d0fbae11c2bee7f
ElgissioFranito <mistera.inwy@gmail.com>
2022-06-08 21:37:39 +0200

    Zone de story

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -1,4 +1,4 @@
 const para = document.createElement('p');           // createElement fa tsy addElement
 const mistera = document.createTextNode('by mistera randriamanarina');
 para.appendChild(mistera);
-facebook.appendChild(para);
+mrbook.appendChild(para);
